8af91af47772fbe05ef6b5a35752d36203af4e97
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-30 11:30:31 -0700

    deleted test tests, added tests for initialize method to check that it creates a Market instance and that all the arguments are stored and accessable

--- lib/market.rb
+++ lib/market.rb
@@ -1,1 +1,17 @@
-DAN_CONSTANT = 42
+module FarMar
+  class Market
+    
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
+    
+    def initialize(id, name, address, city, county, state, zip)
+      @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
+    end
+    
+  end
+end
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,11 +1,52 @@
 require_relative "spec_helper"
 
-describe "tests" do
-  it "should run some tests" do
-    true.must_equal false
+describe "Market" do # describe block for each class
+  
+  describe "#initialize" do # describe block for each function
+    let(:market) {
+      market = FarMar::Market.new(1337, "People's Co-op Farmers Market", "30th and Burnside", "Portland", "Multnomah", "Oregon", "97202")
+    }
+    
+    it "creates an instance of Market" do
+      expect(market).must_be_instance_of FarMar::Market
+    end
+    
+    # verifies initialized Market data was saved.
+    it "keeps track of ID" do
+      expect(market).must_respond_to :id
+      expect(market.id).must_equal 1337
+    end
+    
+    it "keeps track of name" do
+      expect(market).must_respond_to :name
+      expect(market.name).must_equal "People's Co-op Farmers Market"
+    end
+    
+    it "keeps track of address" do
+      expect(market).must_respond_to :address
+      expect(market.address).must_equal "30th and Burnside"
+    end
+    
+    it "keeps track of city" do
+      expect(market).must_respond_to :city
+      expect(market.city).must_equal "Portland"
+    end
+    
+    it "keeps track of county" do
+      expect(market).must_respond_to :county
+      expect(market.county).must_equal "Multnomah"
+    end
+    
+    it "keeps track of state" do
+      expect(market).must_respond_to :state
+      expect(market.state).must_equal "Oregon"
+    end
+    
+    it "keeps track of zip" do
+      expect(market).must_respond_to :zip
+      expect(market.zip).must_equal "97202"
+    end
+    
   end
   
-  it "should have access to stuff from market.rb" do
-    DAN_CONSTANT.must_equal 42
-  end
 end
