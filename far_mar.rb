1d12935227788283981099fd098438b655572c60
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-09-01 17:24:14 -0700

    changed Market to inherit from Loadable

--- far_mar.rb
+++ far_mar.rb
@@ -5,6 +5,7 @@ require 'csv'
 module FarMar; end
 
 # all of our data classes that live in the module
+require_relative "lib/loadable"
 require_relative "lib/product"
 require_relative "lib/vendor"
 require_relative "lib/market"
--- lib/market.rb
+++ lib/market.rb
@@ -1,14 +1,11 @@
 module FarMar
-  class Market
+  class Market < Loadable
     
-    attr_reader :id, :name, :address, :city, :county, :state, :zip
+    attr_reader :name, :address, :city, :county, :state, :zip
     
     def initialize(id, name, address, city, county, state, zip)
-      unless id.instance_of?(Integer) && id > 0
-        raise ArgumentError.new("ID must be a positive Integer (got #{id})")
-      end
+      super(id)
       
-      @id = id
       @name = name
       @address = address
       @city = city
@@ -21,22 +18,20 @@ module FarMar
       return Vendor.all.select { |vendor| vendor.market_id == id}
     end
     
-    def self.all()
-      return CSV.read("support/markets.csv").map do |line|
-        Market.new(
-          line[0].to_i, #id
-          line[1], #name
-          line[2], #address
-          line[3], #city
-          line[4], #county
-          line[5], #state
-          line[6] #zip
-        )
-      end
+    def self.from_csv_line(line)
+      return Market.new(
+        line[0].to_i, #id
+        line[1], #name
+        line[2], #address
+        line[3], #city
+        line[4], #county
+        line[5], #state
+        line[6] #zip
+      )
     end
     
-    def self.find(market_id)
-      return all.find { |market| market.id == market_id }
+    def self.csv_filename()
+      return "support/markets.csv"
     end
     
   end
